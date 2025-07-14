48737f41eee66f2add083d49dcc59f33b35725b7
swarn6402 <swarnjeettiwary01@gmail.com>
2025-07-14 19:33:32 +0530

    WIP add main function

--- Error_handling/yt_manager.py
+++ Error_handling/yt_manager.py
@@ -1,17 +1,46 @@
+import json
 
+def load_data():
+    pass
 
 def list_all_videos(videos):
     pass
 
-while True:
-    print("\n Youtube Manager | Choose an option")
-    print("1. List all Youtube videos")
-    print("2. Add a Youtube video")
-    print("3. Update the Youtube video details")
-    print("4. Delete a Youtube video")
-    print("5. Exit the app")
-    choice = input("Enter your choice")
+def add_video(videos):
+    pass
 
-    match choice:
-        case '1':
-            list_all_videos(videos)
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+def main():
+
+    videos = load_data()
+
+    while True:
+        print("\n Youtube Manager | Choose an option")
+        print("1. List all Youtube videos")
+        print("2. Add a Youtube video")
+        print("3. Update the Youtube video details")
+        print("4. Delete a Youtube video")
+        print("5. Exit the app")
+        choice = input("Enter your choice")
+        
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3' :
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5' :
+                break
+            case _:
+                print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
